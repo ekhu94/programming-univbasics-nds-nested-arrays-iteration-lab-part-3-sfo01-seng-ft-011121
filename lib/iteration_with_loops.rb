210652cb921b67c93bc11f48e0015bd45db5c280
Erik Huang <erikhuangdrums@gmail.com>
2020-12-17 16:49:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,7 @@
 def join_nested_strings(arr)
   words = []
   arr.each do |sub_arr|
-    sub_arr.each { |ele| 
+    sub_arr.each { |ele| words << ele if ele.is_a? String }
   end
+  words.join(" ")
 end
